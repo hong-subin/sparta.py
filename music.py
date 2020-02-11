1391ed0a9fafe64a9fdc9e33527d64134b37104e
subin <hsb5595@gmail.com>
2020-02-11 16:02:40 +0900

    지니뮤직 크롤링 숙제

--- music.py
+++ music.py
@@ -1,11 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
-from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
+from pymongo import MongoClient           # pymongo를 임포트 하기
 client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 
 
-# URL을 읽어서 HTML를 받아오고,
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://www.genie.co.kr/chart/top200?ditc=D&rtm=N&ymd=20190908',headers=headers)
 
